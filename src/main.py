c911099e1e9447545da6d257174ed41959adcd93
andrejhulak <andrejhulak@gmail.com>
2025-09-08 09:25:37 +0200

    changed b size and save dir

--- src/main.py
+++ src/main.py
@@ -9,8 +9,8 @@ from dataset.mosaic_ds import MosaicDataset
 import DINO_4scale as args
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-BATCH_SIZE = 1
-n_epochs = 20
+BATCH_SIZE = 4
+n_epochs = 40
 
 if __name__ == "__main__":
   model, criterion, postprocessors = build_dino(args)
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     total_loss /= len(train_ds)
     print(f"Epoch {epoch}: Total Loss = {total_loss:.4f}")
 
-  torch.save(model.state_dict(), "pth/ddinov3_2.pth")
+  torch.save(model.state_dict(), "pth/ddinov3.pth")
 
   # test_single_image(model, postprocessors, "data/arma/images/frame_0.jpg", device)
-  test_single_image(model, postprocessors, "data/drone_pic.jpg", device)
+  # test_single_image(model, postprocessors, "data/drone_pic.jpg", device)
